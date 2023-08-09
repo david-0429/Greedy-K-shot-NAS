403f559f1437721ef23b561a3cd6d7a20cbd457e
david-0429 <86578317+david-0429@users.noreply.github.com>
2023-08-09 20:14:01 +0900

    Update blocks.py

--- src/Supernet/blocks.py
+++ src/Supernet/blocks.py
@@ -5,7 +5,7 @@ import pdb
 
 class Shufflenet(nn.Module):
 
-    def __init__(self, inp, oup, mid_channels, *, ksize, stride):
+    def __init__(self, inp, oup, mid_channels, *, ksize, stride, iteration):
         super(Shufflenet, self).__init__()
         self.stride = stride
         assert stride in [1, 2]
@@ -17,9 +17,29 @@ class Shufflenet(nn.Module):
         self.pad = pad
         self.inp = inp
 
+        self.iteration = iteration
+
         outputs = oup - inp
 
-        branch_main = [
+        if stride == 2 and iteration == 0:
+          branch_main = [
+            # pw
+            nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            nn.ReLU(inplace=True),
+            # dw
+            nn.Conv2d(mid_channels, mid_channels, ksize,
+                      1, pad, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            # pw-linear
+            nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(outputs, affine=False),
+            nn.ReLU(inplace=True),
+          ]
+          self.branch_main = nn.Sequential(*branch_main)
+
+        else:
+          branch_main = [
             # pw
             nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
             nn.BatchNorm2d(mid_channels, affine=False),
@@ -32,10 +52,23 @@ class Shufflenet(nn.Module):
             nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
             nn.BatchNorm2d(outputs, affine=False),
             nn.ReLU(inplace=True),
-        ]
-        self.branch_main = nn.Sequential(*branch_main)
+          ]
+          self.branch_main = nn.Sequential(*branch_main)
 
-        if stride == 2:
+        if stride == 2 and iteration == 0:
+            branch_proj = [
+                # dw
+                nn.Conv2d(
+                    inp, inp, ksize, 1, pad, groups=inp, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                # pw-linear
+                nn.Conv2d(inp, inp, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                nn.ReLU(inplace=True),
+            ]
+            self.branch_proj = nn.Sequential(*branch_proj)
+
+        elif stride == 2 and iteration == 1:
             branch_proj = [
                 # dw
                 nn.Conv2d(
@@ -50,7 +83,6 @@ class Shufflenet(nn.Module):
 
     def forward(self, old_x):
         if self.stride == 1:
-            #pdb.set_trace()
             x_proj, x = channel_shuffle(old_x)
             return torch.cat((x_proj, self.branch_main(x)), 1)
         elif self.stride == 2:
@@ -61,7 +93,7 @@ class Shufflenet(nn.Module):
 
 class Shuffle_Xception(nn.Module):
 
-    def __init__(self, inp, oup, mid_channels, *, stride):
+    def __init__(self, inp, oup, mid_channels, *, stride, iteration):
         super(Shuffle_Xception, self).__init__()
 
         assert stride in [1, 2]
@@ -71,9 +103,41 @@ class Shuffle_Xception(nn.Module):
         self.ksize = 3
         self.pad = 1
         self.inp = inp
+
+        self.iteration = iteration
+
         outputs = oup - inp
 
-        branch_main = [
+        if self.stride == 2 and self.iteration == 0:
+          branch_main = [
+            # dw
+            nn.Conv2d(inp, inp, 3, 1, 1, groups=inp, bias=False),
+            nn.BatchNorm2d(inp, affine=False),
+            # pw
+            nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            nn.ReLU(inplace=True),
+            # dw
+            nn.Conv2d(mid_channels, mid_channels, 3,
+                      1, 1, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            # pw
+            nn.Conv2d(mid_channels, mid_channels, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            nn.ReLU(inplace=True),
+            # dw
+            nn.Conv2d(mid_channels, mid_channels, 3,
+                      1, 1, groups=mid_channels, bias=False),
+            nn.BatchNorm2d(mid_channels, affine=False),
+            # pw
+            nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
+            nn.BatchNorm2d(outputs, affine=False),
+            nn.ReLU(inplace=True),
+          ]
+          self.branch_main = nn.Sequential(*branch_main)
+
+        else:
+          branch_main = [
             # dw
             nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
             nn.BatchNorm2d(inp, affine=False),
@@ -97,11 +161,22 @@ class Shuffle_Xception(nn.Module):
             nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
             nn.BatchNorm2d(outputs, affine=False),
             nn.ReLU(inplace=True),
-        ]
+          ]
+          self.branch_main = nn.Sequential(*branch_main)
 
-        self.branch_main = nn.Sequential(*branch_main)
+        if self.stride == 2 and self.iteration == 0:
+            branch_proj = [
+                # dw
+                nn.Conv2d(inp, inp, 3, 1, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                # pw-linear
+                nn.Conv2d(inp, inp, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(inp, affine=False),
+                nn.ReLU(inplace=True),
+            ]
+            self.branch_proj = nn.Sequential(*branch_proj)
 
-        if self.stride == 2:
+        elif self.stride == 2 and self.iteration == 1:
             branch_proj = [
                 # dw
                 nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
